62abc9c4b06c69fb4f3964018a1ff61a508c363d
iddokip <kiptarusiddo@gmail.com>
2023-03-16 05:24:18 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle);
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
-		
