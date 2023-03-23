4b9cea5dcc043661c5fad82c4ce825bb70eb5949
iddokip <kiptarusiddo@gmail.com>
2023-03-23 01:06:21 -0700

    multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,18 +5,16 @@
 /* more headers goes there */
 
 /**
- * main - Entry point
- * Description - prints if random generated number is +tive, zero or -tive
+ * positive_or_negative - if random generated number is +tive, zero or -tive
  * Return: Always 0 (success)
+ * @i : intager to be tested
  */
 void  positive_or_negative(int i)
 {
-	
-		
 	if (i < 0)
 		printf("%d is negative\n", i);
 	else if (i == 0)
 		printf("%d is zero\n", i);
 	else
 		printf("%d is positive\n", i);
-}	
+}
