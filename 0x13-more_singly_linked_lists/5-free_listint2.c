82828c23dc433b25ac55a227222a87764d6c6d72
iddokip <kiptarusiddo@gmail.com>
2023-05-02 07:39:54 -0700

    freee

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,26 +1,19 @@
 #include "lists.h"
 
 /**
- * free_listint2 -  function that frees a listint_t list.
- * @head: Head node.
- * Return: result.
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
  */
-
 void free_listint2(listint_t **head)
 {
-	listint_t *tempNode;
-
-	tempNode = *head;
-	if (tempNode == NULL)
-	{
+	listint_t *temp;
+	if (head == NULL)
 		return;
-	}
-	while (tempNode != NULL)
+	while (*head)
 	{
-		listint_t *fr;
-
-		fr = tempNode;
-		tempNode = tempNode->next;
-		free(fr);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 	*head = NULL;
+}
