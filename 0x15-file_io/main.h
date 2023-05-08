d79e0a4106063e3be8239557cbc60bc15907e148
iddokip <kiptarusiddo@gmail.com>
2023-05-08 05:28:46 -0700

    created main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,18 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
+#ifndef MY_MAIN_H
+#define MY_MAIN_H
 
+/* Define buffer size for task 3 */
+#define BUFFER_SIZE 1024
+
+/*
+ * Description: Header file containing prototypes for all functions
+ */
+
+/* Libraries Used */
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/* Prototypes */
 int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-int _pow(int x, int y);
-int _strlen(char *s);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
