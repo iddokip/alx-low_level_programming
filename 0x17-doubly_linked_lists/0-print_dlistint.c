e3c2c7365918bdda30762e24ede4ef33ad7d2150
iddokip <kiptarusiddo@gmail.com>
2023-06-15 02:51:12 -0700

    mm

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Entry Point
+ * Description: Function that prints all the elements of a dlistint_t list
+ * @h: Pointer to the list.
+ * Return: number of nodes.
+ **/
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t count = 0;
+
+	while(current)
+	{
+		printf("%i\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return(count);
+}
+
