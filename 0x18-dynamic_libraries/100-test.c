d315afc2153850ae49bdd53594a4db54470abb11
iddokip <kiptarusiddo@gmail.com>
2023-06-19 03:53:15 -0700

    ffs

---
+++ 0x18-dynamic_libraries/100-test.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract two integers
+int sub(int a, int b) {
+    return a - b;
+}
+
+// Function to multiply two integers
+int mul(int a, int b) {
+    return a * b;
+}
+
+// Function to divide two integers
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    }
+    else {
+        printf("Error: Division by zero!\n");
+        return 0;
+    }
+}
+
+// Function to calculate the modulus of two integers
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    }
+    else {
+        printf("Error: Division by zero!\n");
+        return 0;
+    }
+}
+
